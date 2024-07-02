5dd627d6578457cbc1b68c9a986340f0caf52111
Kalu David <kaludavidinyang@gmail.com>
2024-07-02 18:30:22 +0100

    last commit

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+"use strict";
+
+// Current Day
 const myTime = new Date();
 
-console.log(myTime);
+const day = myTime.getDate();
+
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const today = daysOfWeek[day];
+
+const currentDay = document.getElementById("currentDay");
+
+currentDay.innerHTML = today;
+
+
+
+// CurrentTimeUTC
+
+const utcHours = myTime.getUTCHours();
+const utcMinutes = myTime.getUTCMinutes();
+const utcSeconds = myTime.getUTCSeconds();
+
+const utcCurrentTime = `${utcHours} : ${utcMinutes} : ${utcSeconds} `
+
+const currentTime = document.getElementById("currentTimeUTC");
+currentTime.innerHTML = utcCurrentTime;
